4bb37efa1963dceb31dbb78c64bf4078000e2ffa
Allen Tomlinson <k0reanach0@gmail.com>
2015-04-07 12:13:00 -0500

    added map code

--- ch04.js
+++ ch04.js
@@ -98,6 +98,23 @@ module.exports = JOURNAL;
 
 
 // var journal = [];
+var map = {};
+function storePhi(event, phi) {
+  map[event] = phi;
+}
+
+storePhi("pizza", 0.069);
+storePhi("touched tree", -0.081);
+console.log("pizza" in map);
+// → true
+console.log(map["touched tree"]);
+// → -0.081
+
+for (var event in map)
+console.log("The correlation for '" + event +
+"' is " + map[event]);
+// → The correlation for 'pizza' is 0.069
+// → The correlation for 'touched tree' is -0.081
 
 function addEntry(events, didITurnIntoASquirrel) {
   journal.push({
