db6dbe020856f257dd737f733ba4d7f5a4633d42
Allen Tomlinson <k0reanach0@gmail.com>
2015-04-13 17:30:29 -0500

    higher order functions

--- ch04_playground.js
+++ ch04_playground.js
@@ -215,7 +215,41 @@ function deepEqual(a, b) {
 var obj = {here: {is: "an"}, object: 2};
 // console.log(deepEqual(obj, obj));
 // → true
-console.log(deepEqual(obj, {here: 1, object: 2}));
+// console.log(deepEqual(obj, {here: 1, object: 2}));
 // → false
-console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+// console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
 // → true
+
+function greaterThan(n) {
+  return function(m) { return m > n; };
+}
+var greaterThan10 = greaterThan(10);
+console.log(greaterThan10(11));
+// → true
+
+function noisy(f) {
+  return function(arg) {
+    console.log("calling with", arg);
+    var val = f(arg);
+    console.log("called with", arg, "- got", val);
+    return val;
+  };
+}
+// noisy(Boolean)(0);
+// → calling with 0
+// → called with 0 - got false
+
+function unless(test, then) {
+  if (!test) then();
+}
+function repeat(times, body) {
+  for (var i = 0; i < times; i++) body(i);
+}
+
+// repeat(3, function(n) {
+//   unless(n % 2, function() {
+//     console.log(n, "is even");
+//   });
+// });
+// → 0 is even
+// → 2 is even
