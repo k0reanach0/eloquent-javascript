70b6cfa308e4a4ed99c588ef55b198e3949d3e23
Allen Tomlinson <k0reanach0@gmail.com>
2015-04-13 15:04:03 -0500

    nth recursion

--- ch04_playground.js
+++ ch04_playground.js
@@ -173,5 +173,20 @@ function listToArray(list) {
   return array;
 }
 
+function prepend(value, list) {
+  return {value: value, rest: list};
+}
+
+function nth(list, n) {
+  if (!list)
+    return undefined;
+  else if (n == 0)
+    return list.value;
+  else
+    return nth(list.rest, n - 1);
+}
+
 console.log(arrayToList([10, 20]));
 console.log(listToArray(arrayToList([10, 20, 30])));
+console.log(prepend(10, prepend(20, null)));
+console.log(nth(arrayToList([10, 20, 30]), 2));
