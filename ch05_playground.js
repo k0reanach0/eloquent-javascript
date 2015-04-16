8a6beed0ed74595b7f606e333589b4880644723c
Allen Tomlinson <k0reanach0@gmail.com>
2015-04-16 17:16:26 -0500

    filter and map examples

--- ch05_playground.js
+++ ch05_playground.js
@@ -41,6 +41,25 @@ var ANCESTRY_FILE = "[\n  " + [
 ].join(",\n  ") + "\n]";
 
 var ancestry = JSON.parse(ANCESTRY_FILE);
-console.log(ancestry.filter(function(person) {
-  return person.born > 1900 && person.born < 1925;
+// console.log(ancestry.filter(function(person) {
+//   return person.born > 1900 && person.born < 1925;
+// }));
+
+// console.log(ancestry.filter(function(person) {
+//   return person.died - person.born > 90;
+// }));
+
+function map(array, transform) {
+  var mapped = [];
+  for (var i = 0; i < array.length; i++)
+    mapped.push(transform(array[i]));
+  return mapped;
+}
+
+var overNinety = ancestry.filter(function(person) {
+  return person.died - person.born > 90;
+});
+
+console.log(map(overNinety, function(person) {
+  return person.name + " was " + person.sex.toUpperCase();
 }));
